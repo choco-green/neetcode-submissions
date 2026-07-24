4a787825ba2508d4ba300ef77d2fc23230ece121
choco_green <33989421+choco-green@users.noreply.github.com>
2026-07-24 13:16:09 +0100

    Add: append-characters-to-string-to-make-subsequence - submission-1

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-1.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {number}
+     */
+    appendCharacters(s: string, t: string): number {
+        let sp = 0;
+        let tp = 0;
+        let output = 0;
+
+        while (sp < s.length) {
+            if (s[sp] == t[tp]) {
+                tp++
+            }
+            sp++;    
+        }
+
+        while (tp < t.length) {
+            output++;
+            tp++;
+        }
+
+        return output;
+    }
+}
